99da9a7ec7dcb8ad5fc212d2dfe57fad290cce95
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2024-06-21 12:59:55 +0530

    Book_Management_started

---
+++ server.js
@@ -0,0 +1,29 @@
+const express =require('express')
+const colors = require('colors')
+const moragan = require('morgan')
+const dotenv = require('dotenv')
+
+//config
+dotenv.config()
+
+// rest object 
+    const app = express()
+
+// middleware
+app.use(express.json())
+app.use(moragan('dev'))
+
+//routes
+app.get("/",(res,req)=>{
+    res.status(200).send({
+        message:"server running"
+    })
+})
+
+// port 
+const port  = process.env.PORT || 8080
+//listen
+app.listen(port, () => {
+    console.log(`server is runnig ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`.bgCyan.white)
+})
+
