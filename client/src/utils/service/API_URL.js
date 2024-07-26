17085ce264122615251dda24829cdf888440ad71
Adarsh suryavanshi <adarshsuryavanshi3@gmail.com>
2024-07-26 21:59:24 +0530

    "Updated API_URL.js to add trailing slashes to USER_URL and BOOKS_URL"

--- client/src/utils/service/API_URL.js
+++ client/src/utils/service/API_URL.js
@@ -1,3 +1,3 @@
 // apiConfig.js
-export  const USER_URL = "http://localhost:8080/api/v1/user";
-export const BOOKS_URL = "http://localhost:8080/api/v1/book";
+export  const USER_URL = "http://localhost:8080/api/v1/user/";
+export const BOOKS_URL = "http://localhost:8080/api/v1/book/";
